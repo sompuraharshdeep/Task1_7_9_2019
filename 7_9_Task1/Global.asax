01917c1443e90077ff6dee97be88b5b8c1a22f16
Harshdeep Sompura <sompuraharshdeep@gmail.com>
2019-09-07 23:26:16 +0530

    Add project files.

---
+++ 7_9_Task1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_7_9_Task1.MvcApplication" Language="C#" %>
---
+++ 7_9_Task1/Models/ImageUpload.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace _7_9_Task1.Models
+{
+    public class ImageUpload
+    {
+        public int ImageId { get; set; }
+        //public string ImageTitle { get; set; }
+
+        [DisplayName("Upload Files")]
+        public string ImagePath { get; set; }
+
+        public HttpPostedFileBase ImageFile { get; set; }
+    }
+}
